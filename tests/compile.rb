060698353822a5b847462c0a44e2f553d2976db9
edsloan <edwardsloan@gmail.com>
2015-04-18 13:08:05 +0100

    v1.3.1 basic TravisCI support added

--- tests/compile.rb
+++ tests/compile.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
-result = `sass css/sass/styles.scss css/styles.css`
+result = `sass ../css/sass/styles.scss ../css/styles.css`
 raise result unless $?.to_i == 0
-raise "When compiled the module should output some CSS" unless File.exists?('styles.css')
+raise "When compiled the module should output some CSS" unless File.exists?('../css/styles.css')
 puts "Regular compile worked successfully"
